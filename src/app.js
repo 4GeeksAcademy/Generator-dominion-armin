5e3d2c7af0a52ca3d4026957baf756dbde9d24f8
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-02-16 20:35:35 +0000

    otra solucion

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  
+
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
@@ -23,3 +23,32 @@ window.onload = function() {
       }
   }
 };
+
+// Econtre otra solucion con IA y informacion en internet
+
+// Definimos las partes del dominio
+const pronouns = ["the", "our"];
+const adjectives = ["great", "big"];
+const nouns = ["jogger", "racoon"];
+const extension = ".com";
+
+// Generamos las combinaciones posibles
+// const generateDomains = () => {
+//     let domains = [];
+
+//     // Bucle anidado para combinar todas las palabras
+//     for (let pronoun of pronouns) {
+//         for (let adj of adjectives) {
+//             for (let noun of nouns) {
+//                 let domain = `${pronoun}${adj}${noun}${extension}`;
+//                 domains.push(domain);
+//             }
+//         }
+//     }
+
+//     return domains;
+// };
+
+// // Imprimimos los dominios generados
+// const domains = generateDomains();
+// domains.forEach(domain => console.log(domain));
